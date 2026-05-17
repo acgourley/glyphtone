dc862039896f1a5db5efcdfaeb9feddf06bc5d46
Alex Gourley <acgourley@gmail.com>
2026-05-17 10:26:11 -0700

    config for local hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: './',
+  base: "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     sourcemap: true,
+  },
+  server: {
+    host: true,
+    allowedHosts: true,
   },
 });
